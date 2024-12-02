7883a287867e71a51149ee0e347304daf24f3462
Mani007 <mohnish.a007@gmail.com>
2024-12-02 18:38:16 +0100

    making variable mutable

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
     println!("Hello, world! Its integer datatype");
-    let num:i32 = 25;
-    num=12; 
+    let mut num:i32 = 25;
+    num=12; // we cannot do this unless num is mutable
     println!("this is num value {}", num)
     
 }
