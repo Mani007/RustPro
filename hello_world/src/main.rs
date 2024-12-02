3950a06660ae30c1a24e045e36f3d75fe0e1c450
Mohnish <mohnish.a007@gmail.com>
2024-12-02 21:27:36 +0000

    Basic rust tuple

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,14 +1,6 @@
 fn main() {
     println!("Hello, world! Its &str and String datatype");
-    // String - Dynamic string length and heap allocated
-    // &str - fixed length string and not heap allocated
-    let str_literal: &str = "abcdef";  // imuutable &str type
-    let string_litral1: String = String::from("abcdefgh"); // immitable String type
-    let mut string_litral2: String = String::from("abcdefghijk"); // mutable string literal
-    println!("the string litral is {}", str_literal);
-    println!("the string litral of String type is {}", string_litral1);
-    println!("the string litral of mutable string type is {}", string_litral2);
-    string_litral2.push_str("lmnopqrstuvwxyz"); // adding new string to mutable String type 
-    println!("the string litral of mutable string changed is {}", string_litral2);
-    
+    // Tuples 
+    let emp_info_tuple:(&str, u16) = ("John",25);
+    println!("The name of emplyee is {}, and age is {}",emp_info_tuple.0,emp_info_tuple.1);
 }
