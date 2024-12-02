0c9219bac9d7cf1e05b96bd75b4db00ba7c191e0
Mohnish <mohnish.a007@gmail.com>
2024-12-02 21:42:00 +0000

    Basic rust function with one parameter

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,11 +1,11 @@
 // Functions in Rust
 fn main() {
     println!("Hello, world! Functions in Rust");
-    hello_world();
+    hello_print(5);
 }
 
 // Function declaration
-fn hello_world()  {
+fn hello_print(num:i32)  {
     // code block
-    println!("Hello, world! through the functions in Rust");
+    println!("Hello, world! through the functions in Rust with integer as input of {}", num);
 }
