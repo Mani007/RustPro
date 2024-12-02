aee3b50a022cbb0df3ab36ee44c0f358e88c9422
Mani007 <mohnish.a007@gmail.com>
2024-12-02 18:45:46 +0100

    warning if mutable not needed in a  variable

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,8 +1,9 @@
 fn main() {
     println!("Hello, world! Its integer datatype");
-    let mut num:i32 = 25; // num is now mutable 
+    let mut num:i32 = 25; // num is now mutable
+    let mut num2:i32 = num; 
     // this will throgh a warning as num is changed before using it.
-    println!("this is num value {}", num);
+    println!("this is num value {}", num2);
     num=12; // we can do this as num is mutable
     println!("this is num value {}", num);
     
