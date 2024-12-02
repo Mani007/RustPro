36a06b421bd33a2c973965c85e377b1ace4a7b23
Mani007 <mohnish.a007@gmail.com>
2024-12-02 19:06:47 +0100

    rust &str and String datatype

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,10 +1,14 @@
 fn main() {
-    println!("Hello, world! Its integer datatype");
-    let mut num:i32 = 25; // num is now mutable
-    let num2:i32 = num; 
-    // this will throgh a warning as num is changed before using it.
-    println!("this is num value {}", num2);
-    num=12; // we can do this as num is mutable
-    println!("this is num value {}", num);
+    println!("Hello, world! Its &str and String datatype");
+    // String - Dynamic string length and heap allocated
+    // &str - fixed length string and not heap allocated
+    let str_literal: &str = "abcdef";  // imuutable &str type
+    let string_litral1: String = String::from("abcdefgh"); // immitable String type
+    let mut string_litral2: String = String::from("abcdefghijk"); // mutable string literal
+    println!("the string litral is {}", str_literal);
+    println!("the string litral of String type is {}", string_litral1);
+    println!("the string litral of mutable string type is {}", string_litral2);
+    string_litral2.push_str("lmnopqrstuvwxyz"); // adding new string to mutable String type 
+    println!("the string litral of mutable string changed is {}", string_litral2);
     
 }
