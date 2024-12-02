68dc182d080ff25daae571e5ca6532797f3dc20f
Mohnish <mohnish.a007@gmail.com>
2024-12-02 21:31:10 +0000

    Basic rust tuple extracting data

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -3,4 +3,7 @@ fn main() {
     // Tuples 
     let emp_info_tuple:(&str, u16) = ("John",25);
     println!("The name of emplyee is {}, and age is {}",emp_info_tuple.0,emp_info_tuple.1);
+    let emplyee_name: &str = emp_info_tuple.0;
+    let emplyee_age: u16 = emp_info_tuple.1;
+    println!("The name of employee is by tuple[0] {} and age is by tuple[1] {}",emplyee_name,emplyee_age);
 }
