c1bbd530de7a87c7289b29396cc9fb3b6066ec6c
Mohnish <mohnish.a007@gmail.com>
2024-12-03 16:29:28 +0000

    Reading global constant inside main function

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -8,9 +8,10 @@ fn main() {
     }   
     //println!("Inside variable is {}",inside_variable); // this will create an error because the variable has been out of scope 
     println!("Outside variable is {}",outside_variable); 
-    print_out(); // this will create scope error
+    //print_out(); // this will create scope error
+    println!("try to print Global Constant value {}",SCOPE_CONSTANT)
 }
 
-fn print_out(){
+fn print_out(){  // if you do not use this function rust will through a warning 
     println!("try to print outside variable {}",SCOPE_CONSTANT)
 }
