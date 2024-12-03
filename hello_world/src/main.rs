5eb944e04fd4e025185921ac89421d39dbc2657d
Mohnish <mohnish.a007@gmail.com>
2024-12-03 16:15:25 +0000

    Basic scope programe

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,14 +1,10 @@
-// Functions in Rust
+// Understanding scope in Rust
 fn main() {
-    println!("Hello, world! Functions in Rust");
-    let sum:i32 = hello_add(5,14);
-    println!("Sum of the two numbers is {}",sum);
-}
-
-// Function declaration
-fn hello_add(num1:i32, num2:i32) -> i32 {  // you must specify the return type in the function if required
-    // code block
-    println!("The functions in Rust with two numbers as input");
-    let results:i32 = num1 + num2;
-    return results;
+    let outside_variable:u8 = 5;
+    {
+        let inside_variable:u8 = 10;
+        println!("Inside variable is {}",inside_variable);
+        
+    }   
+    println!("Outside variable is {}",outside_variable); 
 }
