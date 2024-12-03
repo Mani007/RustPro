93d191ef012a83454925ec9264d770f1dfaf5cce
Mohnish <mohnish.a007@gmail.com>
2024-12-03 16:45:44 +0000

    Borrow error in rust and mutation error

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -6,10 +6,11 @@ fn main() {
     // println!("a is {}", a);
     // println!("b is {}", b);
     let srt1:String = String::from("Hello");
-    let str2:String = srt1; // This will through the borrow error as we are dealing with heap and dynamic memory using String
+    srt1.push_str(", World!");
+    //let str2:String = srt1; // This will through the borrow error as we are dealing with heap and dynamic memory using String
     //let str2:String = srt1.clone();
     println!("{}",srt1);
-    println!("{}",str2);
+    //println!("{}",str2);
     /*
     We are dealing with heap memory here and memory is dynamic so when we assign one variable to another it will not copy the value but rather create a reference of that variable. So if you want to make a deep copy then use clone(). This concept is also known as copy trait. We will see traits in details later. 
      */
