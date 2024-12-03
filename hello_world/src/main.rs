0ed4b3dc74ce7ffc6b14fe90f387f224e1c7743d
Mohnish <mohnish.a007@gmail.com>
2024-12-03 16:37:51 +0000

    Borrow error in rust

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,11 +1,15 @@
 // Understanding Ownership in Rust
 
 fn main() {
-    let a:u8 = 15;
-    let b:u8 = a;
-
-    println!("a is {}", a);
-    println!("b is {}", b);
+    // let a:u8 = 15;
+    // let b:u8 = a;
+    // println!("a is {}", a);
+    // println!("b is {}", b);
+    let srt1:String = String::from("Hello");
+    let str2:String = srt1;
+    //let str2:String = srt1.clone();
+    println!("{}",srt1);
+    println!("{}",str2);
    
 }
 
