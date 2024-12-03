b9240a6fa1e4c6b6c95af38204ba1fade33b431c
Mohnish <mohnish.a007@gmail.com>
2024-12-03 16:32:07 +0000

    Basic ownership program

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,17 +1,11 @@
-// Understanding scope in Rust
-const SCOPE_CONSTANT:i32=4; // Declaring global constant, amke sure all letters are capital for global constants
+// Understanding Ownership in Rust
+
 fn main() {
-    let outside_variable:u8 = 5;
-    {
-        let inside_variable:u8 = 10;
-        println!("Inside variable is {}",inside_variable);
-    }   
-    //println!("Inside variable is {}",inside_variable); // this will create an error because the variable has been out of scope 
-    println!("Outside variable is {}",outside_variable); 
-    //print_out(); // this will create scope error
-    println!("try to print Global Constant value {}",SCOPE_CONSTANT)
+    let a:u8 = 15;
+    let b:u8 = a;
+
+    println!("a is {}", a);
+    println!("b is {}", b);
+   
 }
 
-fn print_out(){  // if you do not use this function rust will through a warning 
-    println!("try to print outside variable {}",SCOPE_CONSTANT)
-}
