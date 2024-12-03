868e9ffcf499aaad1aa9658227fc3901d0d18e07
Mohnish <mohnish.a007@gmail.com>
2024-12-03 16:23:38 +0000

    Error when scope is mismanaged again

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -5,6 +5,11 @@ fn main() {
         let inside_variable:u8 = 10;
         println!("Inside variable is {}",inside_variable);
     }   
-    println!("Inside variable is {}",inside_variable); // this will create an error because the variable has been out of scope 
+    //println!("Inside variable is {}",inside_variable); // this will create an error because the variable has been out of scope 
     println!("Outside variable is {}",outside_variable); 
+    print_out(); // this will create scope error
+}
+
+fn print_out(){
+    println!("try to print outside variable {}",outside_variable)
 }
