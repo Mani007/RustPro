e6efeca76c233ec30ba4e3dd2b4a17fa1534f3e4
Mohnish <mohnish.a007@gmail.com>
2024-12-03 16:40:26 +0000

    Borrow error in rust from heap

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -6,7 +6,7 @@ fn main() {
     // println!("a is {}", a);
     // println!("b is {}", b);
     let srt1:String = String::from("Hello");
-    let str2:String = srt1;
+    let str2:String = srt1; // This will through the borrow error as we are dealing with heap and dynamic memory using String
     //let str2:String = srt1.clone();
     println!("{}",srt1);
     println!("{}",str2);
