9ad3b6d8f88af871b7a12f7bb2791c20bea50ce9
Mohnish <mohnish.a007@gmail.com>
2024-12-03 16:46:14 +0000

    Borrow error in rust and but mutation error is resolved

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -5,7 +5,7 @@ fn main() {
     // let b:u8 = a;
     // println!("a is {}", a);
     // println!("b is {}", b);
-    let srt1:String = String::from("Hello");
+    let mut srt1:String = String::from("Hello");
     srt1.push_str(", World!");
     //let str2:String = srt1; // This will through the borrow error as we are dealing with heap and dynamic memory using String
     //let str2:String = srt1.clone();
