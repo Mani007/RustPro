f92d37de76a8edc9daefa6afe6c59581e0fdc04e
Mani007 <mohnish.a007@gmail.com>
2024-12-07 20:08:14 +0100

    Simple string programmewith some completecation but no ownership error

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -4,10 +4,16 @@ fn main() {
     
    let s1:String = get_string();
    println!("The value os string s1 is {}", s1);
+   let s2:String = String::from("WORLD!!");
+   let s3:String = get_new_string(s2);
+   println!("The received string of s3 is {}", s3)
 
 }
 fn get_string() -> String {
     let new_string:String = String::from("Hellloooo");
     return new_string
+}
+fn get_new_string(recevied_string:String) -> String{
+    return recevied_string;
 }
 
