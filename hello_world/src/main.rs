70b584631efc5dd31ac087fc19ce56b03c16fa35
Mani007 <mohnish.a007@gmail.com>
2024-12-07 20:12:18 +0100

    Simple string programme with commet about ownership

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -2,18 +2,18 @@
 
 fn main() {
     
-   let s1:String = get_string();
+   let s1:String = get_string(); // s1 gets the ownership from the function call
    println!("The value os string s1 is {}", s1);
-   let s2:String = String::from("WORLD!!");
-   let s3:String = get_new_string(s2);
+   let s2:String = String::from("WORLD!!");   
+   let s3:String = get_new_string(s2); // s2 ownership transfered to the function get_new_string
    println!("The received string of s3 is {}", s3)
 
 }
 fn get_string() -> String {
-    let new_string:String = String::from("Hellloooo");
+    let new_string:String = String::from("Hellloooo");   // new string variable ownership is transfered to s1 and the variable new_string in the function is dropped from the memory
     return new_string
 }
-fn get_new_string(recevied_string:String) -> String{
+fn get_new_string(recevied_string:String) -> String{  // s2 ownership is transfered here
     return recevied_string;
 }
 
