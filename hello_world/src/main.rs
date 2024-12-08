6a3ef1b1232c4464929f6d7becfd604e221f5466
Mani007 <mohnish.a007@gmail.com>
2024-12-08 23:25:07 +0100

    auto derefereening is not happeing here anymore

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -3,8 +3,8 @@ fn main() {
     let mut x:u8 = 7;
     x=x+1;
     let y = &mut x; // y is referencing to value of x. Don't read it like C/C++ as y is refering to address of x as here its refereing to value of x by auto dereferencing operator. 
-    *y=*y+1;  // x=9 using dereferencing operator to increment x
-    println!("The value of y ref to x is now {}", *y); // custom derefereencing here
+    y=y+1;  // Auto dereferencing is not happening here
+    println!("The value of y ref to x is now {}", y); // custom derefereencing here
     println!("The value of x is now {}", x);
 
 }
