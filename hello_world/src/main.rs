167d3c156cae681c7f12142ccb5eccc7e06ce0b0
Mohnish <mohnish.a007@gmail.com>
2024-12-08 01:56:24 +0000

    Cloning to avoid ownership transfer

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -2,13 +2,13 @@
 
 fn main() {
     let s1:String = String::from("Hello, world!");
-    let (s2,length) = get_string_length(s1); // s1 ownership is transfered to function 
-    println!("The string is {} characters long for the string {}",length, s2);  // ownership is returned in the form of s2 
+    let length:usize = get_string_length(s1.clone()); // s1 ownership is not transferred but its clone is been created
+    println!("The string is {} characters long for the string {}",length, s1);  // ownership of s1 is not transferred and hence s1 is still available in the main function.
 }
 
-fn get_string_length(s:String) -> (String,usize){    // this function takes a string and its ownership and returns a tuple containing the same string with its length as usize type.
+fn get_string_length(s:String) -> usize{    // this function takes a string and but not its ownership and returns a  its length as usize type.
     let s_length:usize = s.len();
-    return (s,s_length);  // returning ownership back to caller in the form of tuple. 
+    return s_length;  // returning ownership back to caller in the form of tuple. 
 }
 
 
