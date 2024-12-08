67a5fe0409d849b653a76cba2ee6f45db688c70e
Mohnish <mohnish.a007@gmail.com>
2024-12-08 02:21:36 +0000

    passing mutable control of the string

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -3,11 +3,11 @@
 fn main() {
     let mut s1:String = String::from("Hello, world!");
     //let length:usize = get_string_length(&s1); // Borrow operation using reference
-    append_str(&mut s1);  // We try to modify original string by borrowing it. 
+    append_str(&mut s1);  // We try to modify original string by borrowing it and passing mutable control. 
     println!("The new string is {} ", s1);  // ownership of s1 is not transferred and hence s1 is still available in the main function.
 }
 
-fn append_str(s: &mut String) {
+fn append_str(s: &mut String) {   // We receive a mutable reference to the string which we can use to modify it.
     s.push_str(" Ya haaa!!");
 }
 // fn get_string_length(s:&String) -> usize{    // this function takes a string reference and its ownership is not transferred to it. It just borrows the value from the calling function and returns a  its length as usize type.
