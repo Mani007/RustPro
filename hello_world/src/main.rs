1035cdb372ac8ee6f75f4a8a8265780ee0dd934d
Mani007 <mohnish.a007@gmail.com>
2024-12-08 20:52:21 +0100

    Auto dereferencing in rust

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -2,6 +2,6 @@
 fn main() {
     let x:i8= 50;
     let y:&i8 = &x; // y is refereence to value of x and value of x is 5
-    println!("TThe value of y refering address of x is {:p}", y);
+    println!("TThe value of y referencing address of x hence the value of x is {}", y); // rust will do auto derefereencing here and hence the value of x
     println!("TThe address of x is at {:p}", &x);
 }
