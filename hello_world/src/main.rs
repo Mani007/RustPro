b62c2d0ceb42cc2b50010a8e210d2b89c52c9461
Mani007 <mohnish.a007@gmail.com>
2024-12-08 20:43:40 +0100

    Update in reffered value

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,6 +1,6 @@
 // Understanding Ownership in Rust
 fn main() {
-    let x:i8= 5;
+    let x:i8= 50;
     let y:&i8 = &x; // y is refereence to value of x and value of x is 5
     println!("The value of y refering to x is {}", y);
 }
