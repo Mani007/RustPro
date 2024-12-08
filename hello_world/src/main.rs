645ae82520ccf08f653e9d867427d4d0e78bb1a1
Mohnish <mohnish.a007@gmail.com>
2024-12-08 02:19:41 +0000

    We need to change mutablity in every reference of the variable in order to change the variable with reference

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -3,11 +3,11 @@
 fn main() {
     let mut s1:String = String::from("Hello, world!");
     //let length:usize = get_string_length(&s1); // Borrow operation using reference
-    append_str(s1);  // We try to modify original string by borrowing it. 
+    append_str(&mut s1);  // We try to modify original string by borrowing it. 
     println!("The new string is {} ", s1);  // ownership of s1 is not transferred and hence s1 is still available in the main function.
 }
 
-fn append_str(s:&String) {
+fn append_str(s: &mut String) {
     s.push_str(" Ya haaa!!");
 }
 // fn get_string_length(s:&String) -> usize{    // this function takes a string reference and its ownership is not transferred to it. It just borrows the value from the calling function and returns a  its length as usize type.
