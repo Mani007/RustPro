18d0e9c8f9f510d059ca165094bfe66a584a447d
Mohnish <mohnish.a007@gmail.com>
2024-12-08 02:17:52 +0000

    We cannot modifiy the borrowed variable directly

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,15 +1,19 @@
 // Understanding Ownership in Rust
 
 fn main() {
-    let s1:String = String::from("Hello, world!");
-    let length:usize = get_string_length(&s1); // Borrow operation using reference
-    println!("The string is {} characters long for the string {}",length, s1);  // ownership of s1 is not transferred and hence s1 is still available in the main function.
+    let mut s1:String = String::from("Hello, world!");
+    //let length:usize = get_string_length(&s1); // Borrow operation using reference
+    append_str(s1);  // We try to modify original string by borrowing it. 
+    println!("The new string is {} ", s1);  // ownership of s1 is not transferred and hence s1 is still available in the main function.
 }
 
-fn get_string_length(s:&String) -> usize{    // this function takes a string reference and its ownership is not transferred to it. It just borrows the value from the calling function and returns a  its length as usize type.
-    let s_length:usize = s.len();
-    return s_length;  // no ownership transfer here.
+fn append_str(s:&String) {
+    s.push_str(" Ya haaa!!");
 }
+// fn get_string_length(s:&String) -> usize{    // this function takes a string reference and its ownership is not transferred to it. It just borrows the value from the calling function and returns a  its length as usize type.
+//     let s_length:usize = s.len();
+//     return s_length;  // no ownership transfer here.
+// }
 
 
 
