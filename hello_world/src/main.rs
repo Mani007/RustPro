aac3ae944a8cf848f4e20200c5525eddf6abcfdd
Mohnish <mohnish.a007@gmail.com>
2024-12-08 01:39:02 +0000

    ownership error resolved and use of usize datatype

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -2,11 +2,11 @@
 
 fn main() {
     let s1:String = String::from("Hello, world!");
-    let length:u8 = get_string_length(s1);
-    println!("The string is {} characters long. and string is {}",length, s1); // clearly this will through error as the ownership of s1 has been moved to function get_string_length
+    let length:usize = get_string_length(s1);
+    println!("The string is {} characters long.",length); // clearly this will through error as the ownership of s1 has been moved to function get_string_length
 }
 
-fn get_string_length(s:String) -> u8{
+fn get_string_length(s:String) -> usize{
     return s.len();
 }
 
