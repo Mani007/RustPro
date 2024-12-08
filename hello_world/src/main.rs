416ef94b2c856114590daf725bdc61f3a71a9b22
Mohnish <mohnish.a007@gmail.com>
2024-12-08 02:07:49 +0000

    Borrow operation by passing the reference but not its ownership

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -2,13 +2,13 @@
 
 fn main() {
     let s1:String = String::from("Hello, world!");
-    let length:usize = get_string_length(s1.clone()); // s1 ownership is not transferred but its clone is been created
+    let length:usize = get_string_length(&s1); // Borrow operation using reference
     println!("The string is {} characters long for the string {}",length, s1);  // ownership of s1 is not transferred and hence s1 is still available in the main function.
 }
 
-fn get_string_length(s:String) -> usize{    // this function takes a string and but not its ownership and returns a  its length as usize type.
+fn get_string_length(s:&String) -> usize{    // this function takes a string reference and its ownership is not transferred to it. It just borrows the value from the calling function and returns a  its length as usize type.
     let s_length:usize = s.len();
-    return s_length;  // returning ownership back to caller in the form of tuple. 
+    return s_length;  // no ownership transfer here.
 }
 
 
