e40f81511b7769129dfdff7b7c324f5efdc50bfd
Mani007 <mohnish.a007@gmail.com>
2024-12-08 18:14:19 +0100

    borrowing for reading purpose only and printing values

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -3,7 +3,9 @@
 fn main() {
     let s1:String = String::from("Hello + ");
     let read1:&String = &s1;  // read1 is borrowing s1 for reading purpose only
+    println!("Value of read1 is {}", read1);
     let read2:&String = &s1;  // read2 is borrowing s1 for reading purpose only
+    println!("Value of read2 is {}", read2);
     println!("The value of read1 is {} and read2 is {}", read1,read2)
 
 }
