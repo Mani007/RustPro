7b0ecea988e2232e88a8be313c6e6d929aa10f28
Mani007 <mohnish.a007@gmail.com>
2024-12-08 20:54:58 +0100

    Auto dereferencing in rust with pointer like syntax in C. Rust is easier without pointer

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -2,6 +2,6 @@
 fn main() {
     let x:i8= 50;
     let y:&i8 = &x; // y is refereence to value of x and value of x is 5
-    println!("TThe value of y referencing address of x hence the value of x is {}", *y); // rust will do auto derefereencing with here and hence the value of x. We just make a C systax here with *pointer
+    println!("TThe value of y referencing address of x hence the value of x is {}", y); // rust will do auto derefereencing with here and hence the value of x. We just make a C systax here with *pointer. Rust is easier without *pointer
     println!("TThe address of x is at {:p}", &x);
 }
