d16b07437286e735e91dc8d7c586bfe4ffe27cc8
Mani007 <mohnish.a007@gmail.com>
2024-12-08 23:23:52 +0100

    custom dereferencing here

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -4,7 +4,7 @@ fn main() {
     x=x+1;
     let y = &mut x; // y is referencing to value of x. Don't read it like C/C++ as y is refering to address of x as here its refereing to value of x by auto dereferencing operator. 
     *y=*y+1;  // x=9 using dereferencing operator to increment x
-    println!("The value of y ref to x is now {}", y);
+    println!("The value of y ref to x is now {}", *y); // custom derefereencing here
     println!("The value of x is now {}", x);
 
 }
