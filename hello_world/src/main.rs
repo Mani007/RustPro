d2a125edb8f88f0ca45ff4107b39ad7299a5314e
Mani007 <mohnish.a007@gmail.com>
2024-12-08 20:25:20 +0100

    This will work as only one mutable reference and one immutable reference

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -8,12 +8,12 @@ fn main() {
     let write1:&mut String = &mut s1;
     write1.push_str(" world");  // we are pushing value one by one hence no error
     //println!("Value of write1 is {}", write1);
-    let write2:&mut String = &mut s1;  // write2 is borrowing s1 for writing purpose
+    let read2: &String = &s1;  // write2 is borrowing s1 for writing purpose
     //let write2:String = write_with2(&mut s1);
     //let write2: &mut String = &mut s1;
     //write2.push_str(" with code."); // We are pushing one by one these values
     //println!("Value of write2 is {}", write2);
-    println!("The value of write1 is {} ", write1) // The moment we call both refernce of s1, although we are just reading, the error will occour
+    println!("The value of read2 is {} ", read2) // The moment we call both refernce of s1, although we are just reading, the error will occour
 }
 // fn write_with1(s:&mut String) -> String{ // passing the borrowed reference of the string
 //     let mut new_string1:String = s.push_str(" World1");
