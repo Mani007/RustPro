451e168676e5ce7c8f60fe028051a4ec7c54747b
Mohnish <mohnish.a007@gmail.com>
2024-12-08 01:48:15 +0000

    Transfering back the ownership using tuple

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -2,12 +2,13 @@
 
 fn main() {
     let s1:String = String::from("Hello, world!");
-    let length:usize = get_string_length(s1);
-    println!("The string is {} characters long.",length); // clearly this will through error as the ownership of s1 has been moved to function get_string_length
+    let (s2,length) = get_string_length(s1); // s1 ownership is transfered to function 
+    println!("The string is {} characters long for the string {}",length, s2);  // ownership is returned in the form of s2 
 }
 
-fn get_string_length(s:String) -> usize{
-    return s.len();
+fn get_string_length(s:String) -> (String,usize){    // this function takes a string and its ownership and returns a tuple containing the same string with its length as usize type.
+    let s_length:usize = s.len();
+    return (s,s_length);  // returning ownership back to caller in the form of tuple. 
 }
 
 
