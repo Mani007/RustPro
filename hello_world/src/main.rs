3c5e6586fe9479a7f9347def5004a9f18a56746c
Mani007 <mohnish.a007@gmail.com>
2024-12-08 18:49:20 +0100

    second mutable borrowing error not coming as we are not calling old value together

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -5,13 +5,13 @@ fn main() {
     let mut s1:String = String::from("Hello + ");
     //let write1:&mut String = &mut s1;  // write1 is borrowing s1 for writing purpose 
     //let write1:String = write_with1(&mut s1);
-    let write1:&mut String = &mut s1;
+    let write1= &mut s1;
     println!("Value of write1 is {}", write1);
     //let write2:&mut String = &mut s1;  // write2 is borrowing s1 for writing purpose 
     //let write2:String = write_with2(&mut s1);
-    let write2:&mut String = &mut s1;
+    let write2 = &mut s1;
     println!("Value of write2 is {}", write2);
-    println!("The value of write1 is {} and read2 is {}", write1,write2)
+   // println!("The value of write1 is {} and read2 is {}", write1,write2)
 }
 // fn write_with1(s:&mut String) -> String{ // passing the borrowed reference of the string
 //     let mut new_string1:String = s.push_str(" World1");
