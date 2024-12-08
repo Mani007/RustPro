0efa856085f13c588ea8669945ffaf2efdd28041
Mani007 <mohnish.a007@gmail.com>
2024-12-08 23:45:30 +0100

    dereference and mut not needed waring

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -3,7 +3,7 @@ fn main() {
     let mut x:u8 = 7;
     x=x+1;
     let mut y = &mut x; // y is referencing to value of x. Don't read it like C/C++ as y is refering to address of x as here its refereing to value of x by auto dereferencing operator. 
-    y=y+1;  // Auto dereferencing is not happening here
+    *y=*y+1;  // dereferencing 
     println!("The value of y ref to x is now {}", y); // custom derefereencing here and through mutablity error
     println!("The value of x is now {}", x);
 
