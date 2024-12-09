c27cfc73d388173593997dff11e28a79e9596cc9
Mohnish <mohnish.a007@gmail.com>
2024-12-09 02:33:53 +0000

    making array by directly passing values

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,6 +1,12 @@
 // Understanding Arrays in Rust
 fn main() {
-    let strarr:[&str;3] = ["Hello","World","Hi!"];
+    let strarr:[&str;3] = ["Hello","World","Hi!"];  // Array is created with directly passing the values of type str and length as 3.
+
+    println!("Length of String Array:{}",strarr.len());
+
+    for i in &strarr{
+        println!("From the loop at index  and value {}",i);
+    }
     println!("Element of string array {}",strarr[0]);
     println!("Element of string array {}",strarr[1]);
     println!("Element of string array {}",strarr[2]);
