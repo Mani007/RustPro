179035e9f8a7cc4f2653a521dfda850dcd7528ab
Mohnish <mohnish.a007@gmail.com>
2024-12-09 02:30:33 +0000

    Fixed:correct way to make string array

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,6 +1,6 @@
 // Understanding Arrays in Rust
 fn main() {
-    let strarr:[&String;3] = ["Hello","World","Hi!"];
+    let strarr:[&str;3] = ["Hello","World","Hi!"];
     println!("Element of string array {}",strarr[0]);
     println!("Element of string array {}",strarr[1]);
     println!("Element of string array {}",strarr[2]);
