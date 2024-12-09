f0b31d0bfb25dd2225f7b0ee107b1316e2d170f1
Mohnish <mohnish.a007@gmail.com>
2024-12-09 02:29:52 +0000

    wrong way to make string array

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,14 +1,8 @@
 // Understanding Arrays in Rust
 fn main() {
-    let a = [1,2,3];  // array type inferred as i32 and length of 3
-    println!("The value of element 0 is {}",a[0]);
-    let b: [i32;5] = [1,2,3,4,5]; // Array type i32 and length of 5
-    println!("The value of element 0 is {}",b[0]);
-    let c = [3;5]; // Array with all elements set to 3
-    println!("The value of element 0 is {}",c[0]);
-    let d:[u8;6] = [1,2,3,4,5,6];
-    println!("The value of element 0 is {}",d[0]);
-    let e = ["Hello","World"]; // String array
-    println!("The value of element 0 is {}",e[0]);
-    println!("The length of array d is {}",d.len());
+    let strarr:[&String;3] = ["Hello","World","Hi!"];
+    println!("Element of string array {}",strarr[0]);
+    println!("Element of string array {}",strarr[1]);
+    println!("Element of string array {}",strarr[2]);
+
 }
