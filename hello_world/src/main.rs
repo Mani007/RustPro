d39d1c13ecd8b6de66d57babeec3a29695cde0e4
Mohnish <mohnish.a007@gmail.com>
2024-12-10 02:43:49 +0000

    array passing by value in function with error due to mutablity

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,14 +1,10 @@
 // Understanding Arrays in Rust
 fn main() {
     let strarr:[&str;3] = ["Hello","World","Hi!"];  // Array is created with directly passing the values of type str and length as 3.
-
-    println!("Length of String Array:{}",strarr.len());
-
-    for i in &strarr{
-        println!("From the loop at index  and value {}",i);
-    }
-    println!("Element of string array {}",strarr[0]);
-    println!("Element of string array {}",strarr[1]);
-    println!("Element of string array {}",strarr[2]);
-
+    write_arr(strarr);
+    println!("The array elements are: {:?}",strarr);  // make sure to use the format specifier of{:?} for printing arrays.
+}
+fn write_arr(temp_arr:[&str;3]) {
+    temp_arr[0] = "foolleo";
+    println!(" temp_arr is {:?}",temp_arr);
 }
