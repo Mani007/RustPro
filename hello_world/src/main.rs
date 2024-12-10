2b5ae3df2debe676770b259db306be57fdecdf72
Mohnish <mohnish.a007@gmail.com>
2024-12-10 02:45:56 +0000

    mutablity error resolved but program execute with warning due to mut multiple time

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,10 +1,10 @@
 // Understanding Arrays in Rust
 fn main() {
-    let strarr:[&str;3] = ["Hello","World","Hi!"];  // Array is created with directly passing the values of type str and length as 3.
+    let mut strarr:[&str;3] = ["Hello","World","Hi!"];  // Array is created with directly passing the values of type str and length as 3.
     write_arr(strarr);
     println!("The array elements are: {:?}",strarr);  // make sure to use the format specifier of{:?} for printing arrays.
 }
-fn write_arr(temp_arr:[&str;3]) {
+fn write_arr(mut temp_arr:[&str;3]) {
     temp_arr[0] = "foolleo";
     println!(" temp_arr is {:?}",temp_arr);
 }
