4f6d5cc708dc194efd471f04d831bf2303cf63bd
Mohnish <mohnish.a007@gmail.com>
2024-12-10 02:46:49 +0000

    mutablity error resolved with no waring and creating multiple copy of array in memory

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,6 +1,6 @@
 // Understanding Arrays in Rust
 fn main() {
-    let mut strarr:[&str;3] = ["Hello","World","Hi!"];  // Array is created with directly passing the values of type str and length as 3.
+    let strarr:[&str;3] = ["Hello","World","Hi!"];  // Array is created with directly passing the values of type str and length as 3.
     write_arr(strarr);
     println!("The array elements are: {:?}",strarr);  // make sure to use the format specifier of{:?} for printing arrays.
 }
