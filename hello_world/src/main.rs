647029c027560f5e7ff65c5eeb2a367ed201f72a
Mohnish <mohnish.a007@gmail.com>
2024-12-10 03:00:27 +0000

    passing array by reference using &mut

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,12 +1,11 @@
 // Understanding Arrays in Rust
 fn main() {
-    let strarr:[&str;3] = ["Hello","World","Hi!"];  // Array is created with directly passing the values of type str and length as 3.
-    write_arr(strarr);
+    let mut strarr:[&str;3] = ["Hello","World","Hi!"];  // Array is created with directly passing the values of type str and length as 3.
+    write_arr_ref(&mut strarr);  // passing array to a function by reference. 
     println!("The array elements are: {:?}",strarr);  // make sure to use the format specifier of{:?} for printing arrays.
 }
-// The function does not lead to same result as new clone of array is getting created inside the function.
-fn write_arr(mut temp_arr:[&str;3]) {  // new copy of strarr is created with name as temp_arr
+// The function make changes in the original array of strarr as it is passed as reference to the function. 
+fn write_arr_ref( temp_arr:&mut [&str;3]) {  // We are taking reference of an array here. hence we need to apply as &mut and size
     temp_arr[0] = "foolleo";
     println!(" temp_arr is {:?}",temp_arr);
 }
-// passing array by value is memory inefficient way of passing array to function. 
