821917f6f340d1bff75cd1f4256a4f12e8b05b4b
Mohnish <mohnish.a007@gmail.com>
2024-12-11 02:52:28 +0000

    Type inference not working in rust

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,20 +1,14 @@
-// Understanding Vectors in Rust
-// Please note that unlike arrays vector is an heap allocated datatype. 
+// Understanding Type inference in Rust
+use std::any::type_name;
 fn main() {
-   let  vrr:Vec<&str> = vec!["abc", "def", "ghi"];   // vrr is the current owner of the vector
 
-   write_vrr( vrr.clone());  // vrr ownership is transfered to the function
-   println!("Strings in the vector is : {:?}",vrr); // Ownership transfered by reference and borrowed back to the caller
+    let x = "hello";
+    let y = 3;
+    let z = 4.8;
 
-}
-fn write_vrr(in_vrr:  Vec<&str>) {   // in_vrr becomes the new owner of the vector
+    println!("x type is {}", type_name(&x));
+    println!("y type is {}", type_name(&y));
+    println!("z type is {}", type_name(&z));
 
-    // println!("Writing to the vector");
-    // in_vrr.push("jkl");
-    // in_vrr.push("mno");
-    // in_vrr.push("pqr");
 
-    // Reading the elements of a vector
-    println!("Reading and printing strings from the vector : {:?}",in_vrr);
-    
 }
