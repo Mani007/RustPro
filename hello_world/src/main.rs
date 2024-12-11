f71a0dab539031aecfe5051e4e7ee93321c2c83b
Mohnish <mohnish.a007@gmail.com>
2024-12-11 02:22:08 +0000

    Vector : push and pop operation on vector

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -3,6 +3,7 @@ fn main() {
    //let mut v:Vec<i32> = Vec::new(); // Vector declaration in rust 
    let mut v = vec![1,2,3,4,5]; // Another way to declare vector in rust
    v.push(6); // Adding elements to vector
+   v.pop(); // Removing elements last index element from vector,here it will remove 6
    println!("Vector is  : {:?}",v);
    
 }
