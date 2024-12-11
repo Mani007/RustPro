07ceacda8619026f999801b33b28d3f1988589f2
Mohnish <mohnish.a007@gmail.com>
2024-12-11 02:55:37 +0000

    Concepy of shadowing in rust and latest value of x is displayed

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,14 +1,12 @@
-// Understanding Type inference in Rust
-use std::any::type_name;
+// Understanding Shadowing in Rust
+
 fn main() {
-
-    let x = "hello";
-    let y = 3;
-    let z = 4.8;
-
-    println!("x type is {}", type_name(&x));
-    println!("y type is {}", type_name(&y));
-    println!("z type is {}", type_name(&z));
+    let x = 30;
+    let x = "hi";
+    let x = x.len();
+    println!("x is {}", x);
+    println!("x is {}", x);
+    
 
 
 }
