259de8fb30dee6648147030e3710e14492aa9bd8
Mohnish <mohnish.a007@gmail.com>
2024-12-11 02:25:50 +0000

    Vector : string vector

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,9 +1,8 @@
 // Understanding Vectors in Rust
+// Please note that unlike arrays vector is an heap allocated datatype. 
 fn main() {
-   //let mut v:Vec<i32> = Vec::new(); // Vector declaration in rust 
-   let mut v = vec![1,2,3,4,5]; // Another way to declare vector in rust
-   v.push(6); // Adding elements to vector
-   v.pop(); // Removing elements last index element from vector,here it will remove 6
-   println!("Vector is  : {:?}",v);
-   
+   let vrr:Vec<&str> = vec!["abc", "def", "ghi"];
+
+   println!("Strings in the vector is : {:?}",vrr);
+
 }
