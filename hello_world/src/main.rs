15db404a4486b4386ee288fd5cbf8c0ae0fd8ef9
Mohnish <mohnish.a007@gmail.com>
2024-12-11 02:20:29 +0000

    Vector : adding an element to vector

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,7 +1,8 @@
 // Understanding Vectors in Rust
 fn main() {
    //let mut v:Vec<i32> = Vec::new(); // Vector declaration in rust 
-   let v = vec![1,2,3,4,5]; // Another way to declare vector in rust
+   let mut v = vec![1,2,3,4,5]; // Another way to declare vector in rust
+   v.push(6); // Adding elements to vector
    println!("Vector is  : {:?}",v);
    
 }
