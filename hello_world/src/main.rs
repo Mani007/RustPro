25b41d484fa8132c63b55fe0442f0a39a39066f5
Mohnish <mohnish.a007@gmail.com>
2024-12-11 02:38:53 +0000

    Vector : ownership and borrowing applied by passing reference

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -3,16 +3,16 @@
 fn main() {
    let  vrr:Vec<&str> = vec!["abc", "def", "ghi"];   // vrr is the current owner of the vector
 
-   write_vrr( vrr);  // vrr ownership is transfered to the function
-   //println!("Strings in the vector is : {:?}",vrr); // This will give error as vrr no longer owns the vector vrr
+   write_vrr( &vrr);  // vrr ownership is transfered to the function
+   println!("Strings in the vector is : {:?}",vrr); // Ownership transfered by reference and borrowed back to the caller
 
 }
-fn write_vrr(in_vrr:  Vec<&str>) {   // in_vrr becomes the new owner of the vector
+fn write_vrr(in_vrr:  &Vec<&str>) {   // in_vrr becomes the new owner of the vector
 
-    println!("Writing to the vector");
-    in_vrr.push("jkl");
-    in_vrr.push("mno");
-    in_vrr.push("pqr");
+    // println!("Writing to the vector");
+    // in_vrr.push("jkl");
+    // in_vrr.push("mno");
+    // in_vrr.push("pqr");
 
     // Reading the elements of a vector
     println!("Reading and printing strings from the vector : {:?}",in_vrr);
