a663482107c1e81cc5bcf4aa058347ca96d55ae7
Mohnish <mohnish.a007@gmail.com>
2024-12-11 02:18:18 +0000

    Vector : another way to declare it

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,6 +1,9 @@
 // Understanding Vectors in Rust
 fn main() {
-   let mut v:Vec<i32> = Vec::new(); // Vector declaration in rust 
+   //let mut v:Vec<i32> = Vec::new(); // Vector declaration in rust 
+   let mut v = Vec::<i32>::new();  // Vector declaration in rust
+
+   v.push(6); // Pushing element to vector
    v.push(5); // Pushing element to vector
    v.push(4); // Pushing element to vector
    v.push(3); // Pushing element to vector
