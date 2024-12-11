5594baa6181569382b73d3be67e39864dd328260
Mohnish <mohnish.a007@gmail.com>
2024-12-11 02:30:56 +0000

    Vector : writing on the vector with clone of its elements

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,13 +1,19 @@
 // Understanding Vectors in Rust
 // Please note that unlike arrays vector is an heap allocated datatype. 
 fn main() {
-   let vrr:Vec<&str> = vec!["abc", "def", "ghi"];
+   let mut vrr:Vec<&str> = vec!["abc", "def", "ghi"];
 
+   write_vrr(&mut vrr);
    println!("Strings in the vector is : {:?}",vrr);
-   read_vrr(vrr);
 
 }
-fn read_vrr(vrr: Vec<&str>) {
+fn write_vrr(vrr: &mut Vec<&str>) {
+    println!("Writing to the vector");
+    vrr.push("jkl");
+    vrr.push("mno");
+    vrr.push("pqr");
+
+    // Reading the elements of a vector
     println!("Reading and printing strings from the vector : {:?}",vrr);
     
 }
