1480e221ef9c11970d9ac9813813c9892c4731db
Mohnish <mohnish.a007@gmail.com>
2024-12-11 02:41:32 +0000

    Vector : passing the clone of the vector

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,18 +1,18 @@
 // Understanding Vectors in Rust
 // Please note that unlike arrays vector is an heap allocated datatype. 
 fn main() {
-   let mut vrr:Vec<&str> = vec!["abc", "def", "ghi"];   // vrr is the current owner of the vector
+   let  vrr:Vec<&str> = vec!["abc", "def", "ghi"];   // vrr is the current owner of the vector
 
-   write_vrr( &mut vrr);  // vrr ownership is transfered to the function
+   write_vrr( vrr.clone());  // vrr ownership is transfered to the function
    println!("Strings in the vector is : {:?}",vrr); // Ownership transfered by reference and borrowed back to the caller
 
 }
-fn write_vrr(in_vrr:  &mut Vec<&str>) {   // in_vrr becomes the new owner of the vector
+fn write_vrr(in_vrr:  Vec<&str>) {   // in_vrr becomes the new owner of the vector
 
-    println!("Writing to the vector");
-    in_vrr.push("jkl");
-    in_vrr.push("mno");
-    in_vrr.push("pqr");
+    // println!("Writing to the vector");
+    // in_vrr.push("jkl");
+    // in_vrr.push("mno");
+    // in_vrr.push("pqr");
 
     // Reading the elements of a vector
     println!("Reading and printing strings from the vector : {:?}",in_vrr);
