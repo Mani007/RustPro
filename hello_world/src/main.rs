be309dc2e2246e6ed1111705eb593cb18df62d12
Mohnish <mohnish.a007@gmail.com>
2024-12-12 23:33:15 +0000

    Loops:for loop in rust

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,11 +1,8 @@
-// Understanding Control Flow in Rust
+// Understanding Loops in Rust
 
 fn main() {
 let my_num = 12;
-if my_num % 3 == 0 && my_num % 4 == 0{
-    println!("my number is divisible by both 3 and 4");
-} else if my_num % 3 == 0 || my_num % 4 == 0 {
-println!("My number is divisible by either three or four, but not both.");
-}else{
-    println!("My number is not divisible by neither three nor four.");}
+for i in 0..my_num{   // for loop in rust
+println!("Hello World");
+}
 }
