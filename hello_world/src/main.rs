b267b72381c2b0b304bcc3e4a56b3479eda7d7dd
Mohnish <mohnish.a007@gmail.com>
2024-12-12 23:40:50 +0000

    Match: match or switch case in rust

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,8 +1,11 @@
-// Understanding Loops in Rust
+// Understanding Match in Rust
 
 fn main() {
-let my_num = [3,2,1];
-for i in &my_num{
-    println!("Array elements are {} ", i);
-}
+    let my_num = 7;
+
+    match my_num {
+        1 => println!("One"),
+        2 | 3 | 5 | 7 | 11 => println!("This is a prime number"), // This will print if the value of my_num matches any of these values.
+        _ => println!("Not special") // This will be printed for all other cases.
+    }
 }
