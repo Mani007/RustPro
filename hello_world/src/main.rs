5df229bd514d7fff5917a01f08b18805cfab6cf4
Mohnish <mohnish.a007@gmail.com>
2024-12-12 23:34:12 +0000

    Loops:simple loop in rust

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,8 +1,18 @@
 // Understanding Loops in Rust
 
 fn main() {
-let my_num = 12;
-for i in 0..my_num{   // for loop in rust
-println!("Hello World");
+let mut my_num = 12;
+loop {
+    if my_num == 0 {
+        break; // Breaks out of the loop when condition is met.
+    } else if my_num % 3 == 0 && my_num % 5 == 0 {
+        println!("FizzBuzz");
+    } else if my_num % 3 == 0 {
+        println!("Fizz");
+    } else if my_num % 5 == 0 {
+        println!("Buzz");
+    }
+
+    my_num -= 1;
 }
 }
