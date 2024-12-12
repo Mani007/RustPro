10b0a08dbadec47b3c4cf2e2a1745f698fee62c3
Mohnish <mohnish.a007@gmail.com>
2024-12-12 23:38:37 +0000

    Loops:for loop on array with reference

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -2,7 +2,7 @@
 
 fn main() {
 let my_num = [3,2,1];
-for i in 0..my_num.len(){
-    println!("Array elements are {} ", my_num[i]);
+for i in &my_num{
+    println!("Array elements are {} ", i);
 }
 }
