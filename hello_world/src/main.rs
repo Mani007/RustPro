826b53d440c5a4aea84906f6d69536e9bbfe258b
Mohnish <mohnish.a007@gmail.com>
2024-12-12 23:37:08 +0000

    Loops:for loop on array

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,9 +1,8 @@
 // Understanding Loops in Rust
 
 fn main() {
-let mut my_num = 12;
-while my_num > 0{
-    println!("{}",my_num);
-    my_num -= 1;
+let my_num = [3,2,1];
+for i in 0..my_num.len(){
+    println!("Array elements are {} ", my_num[i]);
 }
 }
