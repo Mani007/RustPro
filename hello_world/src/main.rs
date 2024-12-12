2d60dbea21d786cffbadb34914ca3b5d2bc06d42
Mohnish <mohnish.a007@gmail.com>
2024-12-12 23:56:46 +0000

    Match: multiple cases

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -4,16 +4,11 @@ fn main() {
     let my_num = 7;
 
     match my_num {
-        1 => println!("One"),
-        2=> println!("Two"),
-        3 => println!("Three"),
-        4 => println!("Four"),
-        5 => println!("Five"),
-        6 => println!("Six"),
-        7 => println!("Seven"),
-        8 => println!("Eight"),
-        9 => println!("Nine"),
-        10 => println!("Ten"),
+        1 | 2 => println!("One or two"),
+        3|4 => println!("Three or four"),
+        5..=9 => println!("Five to nine"),
+        
+        
         _ => println!("Something else")  // default case
     }
 }
