62b0758ae38c44d3abcfbc696c0eb53a736d5b23
Mohnish <mohnish.a007@gmail.com>
2024-12-12 23:34:56 +0000

    Loops:while loop in rust

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -2,17 +2,8 @@
 
 fn main() {
 let mut my_num = 12;
-loop {
-    if my_num == 0 {
-        break; // Breaks out of the loop when condition is met.
-    } else if my_num % 3 == 0 && my_num % 5 == 0 {
-        println!("FizzBuzz");
-    } else if my_num % 3 == 0 {
-        println!("Fizz");
-    } else if my_num % 5 == 0 {
-        println!("Buzz");
-    }
-
+while my_num > 0{
+    println!("{}",my_num);
     my_num -= 1;
 }
 }
