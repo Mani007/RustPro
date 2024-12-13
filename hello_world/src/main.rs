cf9f8ab87bdb80ecb15462d5e9867cd6dc118b81
Mohnish <mohnish.a007@gmail.com>
2024-12-13 00:07:58 +0000

    IO: user input in rust

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,16 +1,14 @@
-// Understanding Match in Rust
-
+// Understanding user input in Rust
+use std::io;
 fn main() {
-    let my_num = 7;
-    // using match statement
-    match my_num {
-        x if is_even(x) => println!("Even"),
-        y if !is_even(y) => println!("Odd"),        
-        _ => println!("Neither even nor odd")  // default case
-    }
+    let mut my_str = String::new();
+    println!("Enter your name: ");
 
+    std::io::stdin()
+        .read_line(&mut my_str)
+        .expect("Failed to read line");
+
+    println!("Hello {}!", my_str);
+    
 }
 
-fn is_even(num: u8) -> bool {
-    return num%2 == 0;
- }
