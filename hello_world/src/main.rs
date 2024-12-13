e3058d5853deb0a01cce0e6d9b1e993fd23e241f
Mohnish <mohnish.a007@gmail.com>
2024-12-13 00:00:50 +0000

    Match: passing function in match

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -3,12 +3,18 @@
 fn main() {
     let my_num = 7;
 
-    match my_num {
-        1 | 2 => println!("One or two"),
-        3|4 => println!("Three or four"),
-        5..=9 => println!("Five to nine"),
-        
-        
-        _ => println!("Something else")  // default case
+    fn is_even(num: u8) -> bool {
+        if num % 2 == 0 {
+            return true;
+        }
+        return false;
     }
+
+    // using match statement
+    match my_num {
+        x if is_even(x) => println!("Even"),
+        y if !is_even(y) => println!("Odd"),        
+        _ => println!("Neither even nor odd")  // default case
+    }
+
 }
