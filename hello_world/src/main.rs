f30779794eaca090bf5b2d28b340fc6155ed0756
Mani007 <mohnish.a007@gmail.com>
2025-01-19 21:35:49 +0100

    Some new code

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,9 +1,7 @@
-// Understanding References in Rust
+// Understanding Advance Datatype in Rust
+// basic scaler types are interger, float, bool, character
 fn main() {
-    let none_ref= create_str_ref(); // dangling reference as local variable cannot return reference
+    
 
 }
-fn create_str_ref() -> &String {
-    let s:String = String::from("HeLLOOO");
-    return &s; // cannot return local variable s reference
-}
+
